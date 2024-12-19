83722ff1ebf779a14c49057de6e667c4abfdcb52
Mahmoud Abu Sultan <muhammadkhan1921@gmail.com>
2024-12-19 04:50:26 +0200

    edit the build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/wills-trust/',
+  base: "./",
   build: {
-    outDir: 'dist', // Default output folder
+    outDir: "dist", // Default output folder
   },
-})
+});
